2429eecf48a4afdf90a4f4366e41039d36ee50d2
Sayantan Karmakar <sayantankarmakar@outlook.com>
2021-05-13 22:17:25 +0530

    refactored SEO

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,6 +13,7 @@ import Skill from "@/components/Skill";
 import Workex from "@/components/Workex";
 import Service from "@/components/Service";
 import Project from "@/components/Project";
+import SEO from "@/components/SEO";
 
 const Bold = ({ children }) => {
   return <span className="font-extrabold">{children}</span>;
@@ -21,80 +22,7 @@ const Bold = ({ children }) => {
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Sayantan Karmakar | Web Developer</title>
-        <meta name="title" content="Sayantan Karmakar | Web Developer" />
-        <meta
-          name="description"
-          content="Freelance Web Developer and UI/UX Designer. Javascript enthusiast."
-        />
-        <meta
-          name="keywords"
-          content="Web Developer, Javascript, Freelance, React, Nextjs, UI/UX, Designer, Front-end"
-        />
-        <meta name="robots" content="index, follow" />
-        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="language" content="English" />
-        <meta name="revisit-after" content="7 days" />
-        <meta name="author" content="Sayantan Karmakar" />
-
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://sayantank.vercel.app/" />
-        <meta property="og:title" content="Sayantan Karmakar | Web Developer" />
-        <meta
-          property="og:description"
-          content="Freelance Web Developer, UI/UX Designer and Javascript enthusiast."
-        />
-        <meta
-          property="og:image"
-          content="https://sayantank.vercel.app/images/sayantank.png"
-        />
-
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://sayantank.vercel.app/" />
-        <meta
-          property="twitter:title"
-          content="Sayantan Karmakar | Web Developer"
-        />
-        <meta
-          property="twitter:description"
-          content="Freelance Web Developer, UI/UX Designer and Javascript enthusiast."
-        />
-        <meta
-          property="twitter:image"
-          content="https://sayantank.vercel.app/images/sayantank.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicons/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicons/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/favicons/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicons/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <link rel="shortcut icon" href="/favicons/favicon.ico" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta
-          name="msapplication-config"
-          content="/favicons/browserconfig.xml"
-        />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
+      <SEO />
       <RegularLayout>
         <div className="flex justify-center lg:justify-end space-x-5">
           <a
